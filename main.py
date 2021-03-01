e91acc116d842cfc423ffda860cf5ff8bbdc1704
amitmaurya7374 <amitmaurya7374@gmail.com>
2021-03-01 10:28:19 +0530

    fetched yesterday and day before yesterday closing price

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
+import requests
+
+from api_keys import ApiKeys
+
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
+keys = ApiKeys()
+
 ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
-# TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
+stock_parameters = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": keys.stockapi
+}
+response = requests.get(url=STOCK_ENDPOINT, params=stock_parameters)
+response.raise_for_status()
+stocks_data = response.json()
+print(stocks_data)
 
-# TODO 2. - Get the day before yesterday's closing stock price
+stock_dict_list = [value for (key, value) in stocks_data.items()]
+stock_dict_list = stock_dict_list[1]
+print(stock_dict_list)
 
-# TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+dates = [key for (key, value) in stock_dict_list.items()]  # list of dates
+print(dates)
 
-# TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+yesterday_closing_price = stock_dict_list[dates[0]]["4. close"]
+print(yesterday_closing_price)
+
+day_before_yesterday_closing_price = stock_dict_list[dates[1]]["4. close"]
+print(day_before_yesterday_closing_price)
+
+# TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
+#  Hint: https://www.w3schools.com/python/ref_func_abs.asp
+
+# TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day
+#  before yesterday.
 
 # TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
 
@@ -22,7 +49,8 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 # TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
 
-# TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+# TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint:
+#  https://stackoverflow.com/questions/509211/understanding-slice-notation
 
 
 ## STEP 3: Use twilio.com/docs/sms/quickstart/python
@@ -34,12 +62,10 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 
 # Optional TODO: Format the message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+"""TSLA: 🔺2% Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. Brief: We at Insider Monkey have 
+gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings 
+show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash. 
+or "TSLA: 🔻5% Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. Brief: We at Insider Monkey 
+have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F 
+filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus 
+market crash. """
