4ebc97b13c37e9d075b3a1b95d3441116ca7397b
amitmaurya7374 <amitmaurya7374@gmail.com>
2021-03-01 12:18:14 +0530

    completed project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from twilio.rest import Client
 
 from api_keys import ApiKeys
 
@@ -18,54 +19,50 @@ stock_parameters = {
     "symbol": STOCK_NAME,
     "apikey": keys.stockapi
 }
+news_parameters = {
+    "q": COMPANY_NAME,
+    "apikey": keys.newsorgapi
+}
 response = requests.get(url=STOCK_ENDPOINT, params=stock_parameters)
 response.raise_for_status()
 stocks_data = response.json()
-print(stocks_data)
 
 stock_dict_list = [value for (key, value) in stocks_data.items()]
 stock_dict_list = stock_dict_list[1]
-print(stock_dict_list)
 
 dates = [key for (key, value) in stock_dict_list.items()]  # list of dates
-print(dates)
 
-yesterday_closing_price = stock_dict_list[dates[0]]["4. close"]
-print(yesterday_closing_price)
+yesterday_closing_price = float(stock_dict_list[dates[0]]["4. close"])
 
-day_before_yesterday_closing_price = stock_dict_list[dates[1]]["4. close"]
+day_before_yesterday_closing_price = float(stock_dict_list[dates[1]]["4. close"])
 print(day_before_yesterday_closing_price)
 
-# TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
-#  Hint: https://www.w3schools.com/python/ref_func_abs.asp
+closing_diff = abs(yesterday_closing_price - day_before_yesterday_closing_price)
 
-# TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day
-#  before yesterday.
+diff_percent = round((closing_diff / float(yesterday_closing_price)) * 100)
+up_down = None
+if diff_percent > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
 
-# TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+diff_percent = 7  # for testing remove it after testing
+if diff_percent > 5:
+    response = requests.get(url=NEWS_ENDPOINT, params=news_parameters)
+    response.raise_for_status()
+    news_data = response.json()["articles"]
+    # news_filter_data = [item[:3] for item in news_data]
+    first_three_news = news_data[0:3]
+    print(first_three_news)
 
-## STEP 2: https://newsapi.org/
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
-
-# TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-
-# TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint:
-#  https://stackoverflow.com/questions/509211/understanding-slice-notation
-
-
-## STEP 3: Use twilio.com/docs/sms/quickstart/python
-# to send a separate message with each article's title and description to your phone number.
-
-# TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-# TODO 9. - Send each article as a separate message via Twilio.
+    headlines = [title["title"] for title in first_three_news]
+    description = [description["description"] for description in first_three_news]
+    client = Client(keys.twilioaccountsid, keys.twilioauthtoken)
+    for i in range(0, 3):
+        message = client.messages.create(
+            body=f"{COMPANY_NAME}{up_down}{diff_percent}% Headline:{headlines[i]}?. Brief: {description[i]}",
+            from_=keys.twiliophonenumber, to=keys.twiliotophonenumber)
 
 
-# Optional TODO: Format the message like this:
-"""TSLA: 🔺2% Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. Brief: We at Insider Monkey have 
-gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings 
-show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash. 
-or "TSLA: 🔻5% Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. Brief: We at Insider Monkey 
-have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F 
-filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus 
-market crash. """
+
+
